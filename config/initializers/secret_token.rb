e5cb96ebac59246f21a776faa12e95dfd03a7366
Dennis Horte <wind.spam@chibikame.net>
2011-02-12 21:51:15 -0800

    Initial commit of default project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTest::Application.config.secret_token = '68d6b2bf5bc2cc74e55422c1290978b621be9642a4fb3445e1d5c9a7ba4722d2924486948e1d56ff17b3716c5632281482654c7ee98c41accbeb973438806dee'
